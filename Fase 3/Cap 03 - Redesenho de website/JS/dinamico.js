8dd166a4fba29a5a75069af38edcd1b7323692ca
Willians Varela <willians.varela@gmail.com>
2018-05-15 17:31:37 -0300

    F3.C6 - Finalizando

--- Fase 3/Cap 03 - Redesenho de website/JS/dinamico.js
+++ Fase 3/Cap 03 - Redesenho de website/JS/dinamico.js
@@ -1,16 +1,12 @@
 //Inicio modal
-// Get the modal
 var modal = document.getElementById('bem-vindo');
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
